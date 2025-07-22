92a002006aa0d438093d167aa16a6a6471b1f04e
Prabhav Manas <prabhavmanas17@gmail.com>
2025-07-22 20:41:55 +0530

    updated-again

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const serverless = require("serverless-http");
 module.exports = serverless(app);
 
 // Local test support
-if (require.main === module) {
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
-}
+// if (require.main === module) {
+//   const port = process.env.PORT || 3000;
+//   app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
+// }
